4172f3857ff6e23ff172dde72a6b5d47803e6046
jvillaflor2 <157553901+jvillaflor2@users.noreply.github.com>
2026-04-10 17:22:35 -0600

    add flask app with SWLAlchemy database models

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Where to put database file
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///budgetbuddy.db'
+
+
+db = SQLAlchemy(app)
+
+# Categorry table
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(10), nullable=False) 
+
+
+# Transaction table
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    date = db.Column(db.String(20), nullable=False)
+    note = db.Column(db.String(200))
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def hello():
