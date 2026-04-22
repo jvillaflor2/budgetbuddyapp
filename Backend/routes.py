d549e6cc77bba3e25909de489db89e6231f76b00
jvillaflor2 <157553901+jvillaflor2@users.noreply.github.com>
2026-04-22 14:22:46 -0600

    add transactions to routes

--- Backend/routes.py
+++ Backend/routes.py
@@ -9,7 +9,7 @@ def get_categories():
     categories = Category.query.all()
     return jsonify([{ 'id': c.id, 'name': c.name, 'type': c.type } for c in categories])
 
-#POST/
+#POST/ categories
 @bp.route('/categories', methods = ['POST'])
 def add_category():
     data = request.get_json()
@@ -18,10 +18,50 @@ def add_category():
     db.session.commit()
     return jsonify({ 'id': category.id, 'name': category.name, 'type': category.type }) 
 
-#DELETE
+#DELETE/ categories
 @bp.route('/categories/<int:id>', methods=['DELETE'])
 def delete_category(id):    
     category = Category.query.get_or_404(id)
     db.session.delete(category)
+    db.session.commit()
+    return jsonify({'message': 'Deleted'}), 200
+
+#GET /transactions
+@bp.route('/transactions', methods=['GET'])
+def get_transactions():
+    transactions = Transaction.query.all()
+    return jsonify([{
+        'id': t.id,
+        'amount': t.amount,
+        'category_id': t.category_id,
+        'date': t.date,
+        'note': t.note
+    } for t in transactions])
+
+#POST /transactions
+@bp.route('/transactions', methods=['POST'])
+def add_transaction():
+    data = request.get_json()
+    transaction = Transaction(
+        amount=data['amount'],
+        category_id=data['category_id'],
+        date=data['date'],
+        note=data.get('note', '')
+    )
+    db.session.add(transaction)
+    db.session.commit()
+    return jsonify({
+        'id': transaction.id,
+        'amount': transaction.amount,
+        'category_id': transaction.category_id,
+        'date': transaction.date,
+        'note': transaction.note
+    }), 201
+
+#DELETE /transactions
+@bp.route('/transactions/<int:id>', methods=['DELETE'])
+def delete_transaction(id):
+    transaction = Transaction.query.get_or_404(id)
+    db.session.delete(transaction)
     db.session.commit()
     return jsonify({'message': 'Deleted'}), 200
