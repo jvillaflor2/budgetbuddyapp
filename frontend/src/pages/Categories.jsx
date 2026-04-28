fcc01952cb492274d4d3a7589aedba456065e7a7
jvillaflor2 <157553901+jvillaflor2@users.noreply.github.com>
2026-04-28 11:49:03 -0600

    add Categories page with fetch add and delete functions

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 
-function Categories() {
+function Categories(){
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState('');
+  const [type, setType] = useState('expense');
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  
+  const fetchCategories = async() => {
+    const response = await axios.get('http://localhost:5000/categories');
+    setCategories(response.data);
+
+  }
+
+  const addCategory = async () => {
+    await axios.post('http://localhost:5000/categories', { name, type });
+    setName('');
+    fetchCategories();
+  };
+
+  const deleteCategory = async (id) => {
+    await axios.delete(`http://localhost:5000/categories/${id}`);
+    fetchCategories();
+  };
   return <h1>Categories</h1>;
 }
 
