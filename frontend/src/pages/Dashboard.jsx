1d9ad467b8b77324fec772ed8e00096a4721705f
jvillaflor2 <157553901+jvillaflor2@users.noreply.github.com>
2026-05-21 21:55:23 -0600

    add Dashboard page in progress - imports and  useEffect

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, {useState, useEffect}from 'react';
+import axios from 'axios';
 
 function Dashboard() {
+
+  const [transactions, setTransactions] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetchTransactions();
+    fetchCategories();
+}, []);
   return <h1>Dashboard</h1>;
 }
 
